64691652562f6e458e7a884be5d496fad443781f
Jonathan <jonataanthony@gmail.com>
2020-06-09 19:41:13 -0300

    Complete smooth scroll

--- app.js
+++ app.js
@@ -5,4 +5,20 @@ $(".section-features").waypoint(function(direction){
     else{
         $('#menu').removeClass('sticky');
     }
+},{
+    offset: '10%'
+});
+
+$('.scroll-to-plans').click(function(){
+    $('html, body').animate({scrollTop: $('#plans').offset().top}, 1000);
+});
+
+$('.scroll-to-start').click(function(){
+    $('html, body').animate({scrollTop: $('#features').offset().top}, 1000);
+});
+
+
+$('a[href*=\\#]').on('click', function(event){     
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
 });
